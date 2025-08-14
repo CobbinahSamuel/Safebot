56803975483915c94e20368d70e8c906dea24c5e
Jeffery Ahodokpo <ahodokpojeffery@gmail.com>
2025-08-14 22:52:43 +0000

    set up backend server

---
+++ backend/models/userModels.js
@@ -0,0 +1,57 @@
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
+    },
+    
+    password: {
+      type: String,
+      minLength: [8, "Password must be at least 8 characters"],
+    },
+    
+    role: {
+      type: String,
+      default: "admin",
+      enum: ["admin", "users"],
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare passwords
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
+
+const User = mongoose.model("User", userSchema);
+export default User;
